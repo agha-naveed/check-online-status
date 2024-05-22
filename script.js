09782fcff80b02a2b6d042df3a53409c6cc6d061
Agha Naveed <naveedabs31@gmail.com>
2024-05-22 18:56:15 +0500

    Deleted a Front Message Changes

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let popup = document.querySelector('.no-con-popup')
 let popupUpperLine = document.querySelector('.no-con-popup > span')
 let popupCountdown = document.querySelector('.popup-texts > p > b')
-let frontMessageH1 = document.querySelector('.no-con-front-message > h1')
-let frontMessageH2 = document.querySelector('.no-con-front-message > h2')
+
 let count = 1
 let popupHeading = document.querySelector('.popup-texts > h2')
 let popupPara = document.querySelector('.popup-texts > p')
@@ -15,8 +14,6 @@ function checkConnection() {
         popupIcn.style.backgroundColor = `#E54E65`
         popupHeading.innerHTML = `Lost Connection`
         popupPara.innerHTML = `Your network is unavailable. We will attempt to reconnect you in <b>10</b> seconds.`
-        frontMessageH1.innerHTML = `Check Your Internet Connection`
-        frontMessageH2.innerHTML = `Try Rebooting your Internet Device...`
         // setInterval(() => {
         //     if(popupCountdown.innerText == 0) {
         //         popupCountdown.innerHTML = 11
@@ -32,8 +29,7 @@ function checkConnection() {
             popupPara.innerHTML = 'Your Device is now Successfully Connected to the Internet'
             popupIcn.style.backgroundColor = '#61b75b'
             popupUpperLine.style.backgroundColor = '#61b75b'
-            frontMessageH1.innerHTML = `Internet Connection is Restored!`
-            frontMessageH2.innerHTML = `...`
+
             setTimeout(() => popup.style.top = '-50%', 3000)
         }
     }
