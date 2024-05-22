2183de0b949f4a2541090c17cc66f9c82fc6c47e
Agha Naveed <naveedabs31@gmail.com>
2024-05-22 18:50:33 +0500

    Default Front Message Fixed

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ function checkConnection() {
         popupIcn.style.backgroundColor = `#E54E65`
         popupHeading.innerHTML = `Lost Connection`
         popupPara.innerHTML = `Your network is unavailable. We will attempt to reconnect you in <b>10</b> seconds.`
-        frontMessageH1.innerHTML = `Internet Connection is Restored!`
-        frontMessageH2.innerHTML = `...`
+        frontMessageH1.innerHTML = `Check Your Internet Connection`
+        frontMessageH2.innerHTML = `Try Rebooting your Internet Device...`
         // setInterval(() => {
         //     if(popupCountdown.innerText == 0) {
         //         popupCountdown.innerHTML = 11
@@ -33,8 +33,8 @@ function checkConnection() {
             popupPara.innerHTML = 'Your Device is now Successfully Connected to the Internet'
             popupIcn.style.backgroundColor = '#61b75b'
             popupUpperLine.style.backgroundColor = '#61b75b'
-            frontMessageH1.innerHTML = `Check Your Internet Connection`
-            frontMessageH2.innerHTML = `Try Rebooting your Internet Device...`
+            frontMessageH1.innerHTML = `Internet Connection is Restored!`
+            frontMessageH2.innerHTML = `...`
             setTimeout(() => popup.style.top = '-50%', 3000)
         }
     }
